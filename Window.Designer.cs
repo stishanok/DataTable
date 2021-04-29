34c3c307c3ed408258cf454b3a1ce6755372f60a
Vadim Stishanok <stishanok@gmail.com>
2021-04-29 05:12:38 +0300

    Add project files.

---
+++ Window.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace DataTable
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textCommand = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSend.Location = new System.Drawing.Point(634, 12);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(74, 67);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.ShowClick);
+            // 
+            // textCommand
+            // 
+            this.textCommand.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textCommand.Location = new System.Drawing.Point(12, 12);
+            this.textCommand.Multiline = true;
+            this.textCommand.Name = "textCommand";
+            this.textCommand.Size = new System.Drawing.Size(615, 67);
+            this.textCommand.TabIndex = 1;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 98);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(696, 441);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 551);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.textCommand);
+            this.Controls.Add(this.buttonSend);
+            this.Name = "Window";
+            this.Text = "DataTable MS SQL Sever";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textCommand;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ Window.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Configuration;
+
+
+namespace DataTable
+{
+    public partial class Window : Form
+    {
+
+        private SqlDataReader reader;
+        private System.Data.DataTable table;
+        private SqlConnection connection;
+
+        public Window()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString);
+        }
+
+        private void ShowClick(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand(textCommand.Text, connection);
+                dataGridView.DataSource = null;
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                System.Data.DataTable table = new System.Data.DataTable();
+                int line = 0;
+
+                while (reader.Read())
+                {
+                    if (line == 0)
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            table.Columns.Add(reader.GetName(i));
+                        }
+
+                        line++;
+                    }
+
+                    DataRow row = table.NewRow();
+
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        row[i] = reader[i];
+                    }
+
+                    table.Rows.Add(row);
+                }
+
+                dataGridView.DataSource = table;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Wrong request syntax");
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+            
+        }
+    }
+}
